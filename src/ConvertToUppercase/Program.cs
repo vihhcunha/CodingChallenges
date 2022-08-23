4d667d9b6b672d3c90fb9ee1ee9a35c979f71d41
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2022-08-23 20:33:33 -0300

    add convert to uppercase project

---
+++ src/ConvertToUppercase/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+Console.WriteLine(" ------------------------------------------------");
+Console.WriteLine(" Write a sentence and see convert it to uppercase");
+Console.WriteLine(" ------------------------------------------------");
+
+var sentence = Console.ReadLine();
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+
+stopwatch.Stop();
+
+var newSentence = ConvertToUppercase(sentence);
+
+Console.WriteLine("\nNew sentence: " + newSentence);
+Console.WriteLine("\n\nElapsed time in milliseconds: " + stopwatch.ElapsedMilliseconds);
+
+static string ConvertToUppercase(string? sentence)
+{
+    var sentenceASCIIBytes = Encoding.ASCII.GetBytes(sentence);
+    var newByteArraySentence = new byte[sentenceASCIIBytes.Length];
+
+    for (var i = 0; i < sentenceASCIIBytes.Length; i++)
+    {
+        if (sentenceASCIIBytes[i] >= 'a' && sentenceASCIIBytes[i] <= 'z')
+        {
+            newByteArraySentence[i] = (byte)(sentenceASCIIBytes[i] - ('a' - 'A'));
+        }
+        else
+        {
+            newByteArraySentence[i] = sentenceASCIIBytes[i];
+        }
+    }
+
+    return Encoding.ASCII.GetString(newByteArraySentence);
+}
